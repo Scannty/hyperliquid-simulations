5410e43a6db5ed18a32e837170dbde6f248979b9
Aleksa Uroseivc <aleksa.urosevic0@gmail.com>
2025-08-22 23:50:12 +0200

    feat: Add anvil service

---
+++ back-end/src/anvil/service.go
@@ -0,0 +1,42 @@
+package anvil
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+type anvilService interface {
+	StartAnvilProcess(port int, rpcUrl string) error
+	StopAnvilProcess(port int) error
+}
+
+type Service struct {
+	portToCmdMap map[int]*exec.Cmd
+}
+
+func NewService() *Service {
+	return &Service{portToCmdMap: make(map[int]*exec.Cmd)}
+}
+
+func (s *Service) StartAnvilProcess(port int, rpcUrl string) error {
+	cmd := exec.Command("anvil", "--steps-tracing", "--port", fmt.Sprint(port), "--host", "0.0.0.0", "--fork-url", rpcUrl)
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	s.portToCmdMap[port] = cmd
+	return nil
+}
+
+func (s *Service) StopAnvilProcess(port int) error {
+	cmd := s.portToCmdMap[port]
+
+	err := cmd.Process.Kill()
+	if err != nil {
+		return err
+	}
+
+	delete(s.portToCmdMap, port)
+	return nil
+}
